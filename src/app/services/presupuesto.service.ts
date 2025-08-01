5dbbba6da0b7b1bbd9e49976316c2e7033026451
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-08-01 12:53:26 -0600

    Desarrollo de funciones de Gastos

--- src/app/services/presupuesto.service.ts
+++ src/app/services/presupuesto.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,23 @@ export class PresupuestoService {
 
   presupuesto: number;
   restante: number;
+  private gastos$ = new Subject<any>();
+
   constructor() { 
     this.presupuesto = 0;
     this.restante = 0;
+  }
+
+  // Metodo que emite datos 
+  
+  agregarGasto(gasto: any) {
+    this.restante = this.restante - gasto.cantidad;
+    this.gastos$.next(gasto);
+  }
+
+  /* A este metodo se van a suscribir todos los componentes
+      que quieran escuchar los cambios de gasto$  */
+  getGastos(): Observable<any> {
+    return this.gastos$.asObservable();
   }
 }
